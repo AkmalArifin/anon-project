97f2a679d7f9c848d9090534992ac940278df1d2
Akmal Arifin <makmalarifin25@gmail.com>
2024-09-10 13:05:57 +0700

    feat: init backend

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	err := godotenv.Load()
+
+	if err != nil {
+		panic("could not load .env")
+	}
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hellow World!"})
+	})
+
+	port := ":" + os.Getenv("PORT")
+	r.Run(port)
 }
