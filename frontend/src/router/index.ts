97fd6bfa65a7e933adaca001a1ef3c3269d344da
Akmal Arifin <makmalarifin25@gmail.com>
2024-09-12 11:16:24 +0700

    feat: login

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AskView from '@/views/AskView.vue'
 import DashboardView from '@/views/DashboardView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
     },
     {
       path: '/ask/:username',
