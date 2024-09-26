22e22e817c29092e49e230cdce073aad1b2cfd36
Muhammad Akmal Arifin <makmalarifin25@gmail.com>
2024-09-26 09:35:05 +0700

    feat: basic dashboard

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,10 +1,41 @@
 <template>
     <div>
-        <h1>Dashboard</h1>
+        <div>
+            <p v-for="q in questions" :key="q.id">{{q.question}}</p>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import axios from 'axios';
+import { ref } from 'vue'
+
+interface Question {
+    id: number;
+    user_id: number;
+    question: string;
+    is_read: boolean;
+    created_at: string;
+}
+
+const questions = ref<Question[]>()
+
+const token = sessionStorage.getItem('jwtToken');
+
+axios.get("http://localhost:8082/ask-log", {
+    headers: {
+        'Authorization': `Bearer ${token}`
+    }
+}).then(response => {
+    questions.value = response.data
+    console.log(questions.value)
+}).catch(error => {
+    console.error(error)
+})
+
+async function getData(event: Event) {
+    
+}
 
 </script>
 
