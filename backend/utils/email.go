11a33d86e0eb276569eaf3251865c7c95d6686b2
Muhammad Akmal Arifin <makmalarifin25@gmail.com>
2024-10-29 11:23:35 +0700

    feat: email test

---
+++ backend/utils/email.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
+
+func HelloWorldEmail(toName, toEmail string) {
+	from := mail.NewEmail("Hnn Admin", "sudahakmal@gmail.com")
+	subject := "Sending with SendGrid is Fun"
+	to := mail.NewEmail(toName, toEmail)
+	plainTextContent := "and easy to do anywhere, even with Go"
+	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	response, err := client.Send(message)
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(response.StatusCode)
+		fmt.Println(response.Body)
+		fmt.Println(response.Headers)
+	}
+}
