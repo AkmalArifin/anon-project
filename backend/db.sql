83b3273b096818df1032644a00529b91990ede05
Muhammad Akmal Arifin <makmalarifin25@gmail.com>
2024-11-14 21:10:08 +0700

    feat: add env example and db

---
+++ backend/db.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `users` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(255) UNIQUE,
+  `email` varchar(255) UNIQUE,
+  `password` varchar(255),
+  `photo_profile` varchar(255),
+  `created_at` datetime,
+  `updated_at` datetime,
+  `deleted_at` datetime
+);
+
+CREATE TABLE `log` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `user_id` integer,
+  `message` varchar(255),
+  `is_starred` integer,
+  `created_at` datetime
+);
+
+ALTER TABLE `log` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
